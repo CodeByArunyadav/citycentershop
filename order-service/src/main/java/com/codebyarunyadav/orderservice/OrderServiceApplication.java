8da9ef8301dcfe2f437b7b60a381081b989ad92c
Arun Yadav <Arun Yadav@DESKTOP-P66ORNM.mshome.net>
2023-12-02 14:56:19 +0530

    Eureka server with 12 files had been added first time

--- order-service/src/main/java/com/codebyarunyadav/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/codebyarunyadav/orderservice/OrderServiceApplication.java
@@ -2,6 +2,7 @@ package com.codebyarunyadav.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,7 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableJpaRepositories
 @EnableFeignClients
 @EnableWebSecurity
