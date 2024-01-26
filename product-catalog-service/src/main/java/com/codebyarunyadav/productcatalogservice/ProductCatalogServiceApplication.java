552359ee15bf3b65d6683e1092a694f736f8961d
Arun Yadav <Arun Yadav@DESKTOP-P66ORNM.mshome.net>
2024-01-26 18:41:38 +0530

    Eureka-client annotation enable and dependency uncommented

--- product-catalog-service/src/main/java/com/codebyarunyadav/productcatalogservice/ProductCatalogServiceApplication.java
+++ product-catalog-service/src/main/java/com/codebyarunyadav/productcatalogservice/ProductCatalogServiceApplication.java
@@ -2,11 +2,11 @@ package com.codebyarunyadav.productcatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient; 
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-// @EnableEurekaClient
+@EnableEurekaClient
 @EnableJpaRepositories
 public class ProductCatalogServiceApplication {
     public static void main(String[] args) {
