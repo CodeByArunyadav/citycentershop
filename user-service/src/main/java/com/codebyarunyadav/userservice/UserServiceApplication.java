3bf023f7537cad479e22c989ae504a4fe50fb5a3
Arun Yadav <Arun Yadav@DESKTOP-P66ORNM.mshome.net>
2024-01-26 12:21:48 +0530

    eureka property has been updated

--- user-service/src/main/java/com/codebyarunyadav/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/codebyarunyadav/userservice/UserServiceApplication.java
@@ -1,12 +1,15 @@
 package com.codebyarunyadav.userservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-// @EnableEurekaClient
+@EnableEurekaClient
+
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
